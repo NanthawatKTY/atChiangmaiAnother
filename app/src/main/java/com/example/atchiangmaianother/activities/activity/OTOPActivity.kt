4a1c51e148c2985aaa1c85dd4bf359ce331d9873
NanthawatKTY <nanthawat.tae123@gmail.com>
2021-01-12 19:03:06 +0700

    Added OTOP Activity

--- app/src/main/java/com/example/atchiangmaianother/activities/activity/OTOPActivity.kt
+++ app/src/main/java/com/example/atchiangmaianother/activities/activity/OTOPActivity.kt
@@ -2,6 +2,9 @@ package com.example.atchiangmaianother.activities.activity
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.atchiangmaianother.R
 
@@ -11,6 +14,31 @@ class OTOPActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otop)
 
-        this.title = "OTOP"
+        //Action Bar - OTOP
+        val actionbarOTOP = supportActionBar
+
+        //Set ACtion Title
+        actionbarOTOP!!.title = "OTOP"
+
+        //Set back Button
+        actionbarOTOP.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when(item.itemId){
+//           // R.id.icBack -> Toast.makeText(this, "Get Back Fuck", Toast.LENGTH_SHORT).show()
+//            R.id.icCart -> Toast.makeText(this, "Get Go Cart Fuck", Toast.LENGTH_SHORT).show()
+//        }
+//        return true
+//    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
